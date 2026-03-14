49c34dc4139b870f9ad105bed70f84ff3fe6901c
Yourstruggle11 <sensouvik51@gmail.com>
2026-03-14 06:18:25 +0530

    `fix`: Fixed middle sidenavbar broken dynamic height issue

--- src/desktop/Route.jsx
+++ src/desktop/Route.jsx
@@ -36,9 +36,9 @@ function DesktopRouting() {
           <Route
             path="*"
             element={
-              <div className="flex">
+              <div className="flex min-w-0">
                 <Sidebarpart />
-                <div className="flex-1 border border-orange-400 min-h-screen">
+                <div className="min-w-0 flex-1 border border-orange-400 min-h-screen">
                   <Searchbar />
                   <Routes>
                     <Route path="/home" element={<Home />} />
